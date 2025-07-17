2ef322d035bb438bd6f4ebaae0944df2588556a7
Anas <syriacano@gmail.com>
2025-07-17 16:35:00 +0200

    fix bug inviornment for deployment

--- client/src/config/axiosConfig.js
+++ client/src/config/axiosConfig.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_APP_BELSY_API_URL || 'http://localhost:3000/api',
-  withCredentials: true,    
+  baseURL: import.meta.env.VITE_API_BASE_URL ,
+  withCredentials: true,
 });
 
 axiosInstance.interceptors.request.use((config) => {

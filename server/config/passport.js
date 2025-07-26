af5fbad3a571adcf244921926ccead93c8c35ab6
Anas <syriacano@gmail.com>
2025-07-26 15:43:26 +0200

    fix passport path

--- server/config/passport.js
+++ server/config/passport.js
@@ -17,13 +17,14 @@ passport.deserializeUser(async (id, done) => {
   }
 });
 
+const base = 'https://belsy-api.onrender.com';
 // 🔐 Google OAuth Strategy
 passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: `${process.env.BACKEND_URL}/api/auth/google/callback`
+      callbackURL: `${base}/api/auth/google/callback`
     },
     async (accessToken, refreshToken, profile, done) => {
       try {

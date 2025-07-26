18a2310a200776cc666e5973ffc5f4ca1eae58d6
Anas <syriacano@gmail.com>
2025-07-26 13:28:24 +0200

    fix google login issue

--- server/config/passport.js
+++ server/config/passport.js
@@ -19,7 +19,7 @@ passport.use(new GoogleStrategy(
   {
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: `${process.env.BACKEND_URL}/api/auth/google/callback`,
+    callbackURL: `${process.env.SERVER_URL}/api/auth/google/callback`,
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
